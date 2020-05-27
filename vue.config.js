5e3eb434b021ee2fd55b3344ece1e73a60e2d61d
REMMANI <remmani@v12software.com>
2020-05-27 16:17:30 +0000

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+const path =require('path')
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/gestionstck/'
+  : '/',
+  outputDir:path.resolve(__dirname,"../server/public"),
+  devServer:{
+   proxy:{
+     '/api':{
+       target:'http://localhost:3000'
+     }
+   }
+  },
   "transpileDependencies": [
     "vuetify"
   ]
